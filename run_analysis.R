fd1064612f4301ab3b7816b047ef8f55e52e23e6
clembl76 <clembl76@users.noreply.github.com>
2014-06-22 23:39:51 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,8 +4,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 #Set working directory
-setwd("/Volumes/DANE ELEC/data scientist toolbox/R-wd/ProgrammingAssignement3") #mac
-#setwd("G:/data scientist toolbox/R-wd/ProgrammingAssignement3") #pc
+setwd("G:/data scientist toolbox/R-wd/ProgrammingAssignement3") #pc
 
 #Data is unzipped in the working directory, in folder /UCI HAR Dataset
 
@@ -25,17 +24,17 @@ names(activity_labels)<-c("activityCode","activityLabel")
 # load data
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")#,nrows=20) #,skip=4,na.strings = c("..",""), stringsAsFactors=FALSE)
-X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")#,nrows=20)
-Y_test<-read.table("./UCI HAR Dataset/test/Y_test.txt")#,nrows=20)
+subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+Y_test<-read.table("./UCI HAR Dataset/test/Y_test.txt")
 names(subject_test)<-"subject"
 names(Y_test)<-"activityCode"
 
-subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")#,nrows=20)
-X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")#,nrows=20)
-Y_train<-read.table("./UCI HAR Dataset/train/Y_train.txt")#,nrows=20)
+subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+Y_train<-read.table("./UCI HAR Dataset/train/Y_train.txt")
 names(subject_train)<-"subject"
-names(Y_train)<-"activityCode" # "label"
+names(Y_train)<-"activityCode"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #merge train and test data sets
@@ -49,9 +48,15 @@ all<-rbind(test,train)
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Extracts only the measurements on the mean and standard deviation for each measurement. 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# columns names are located in features[,2]
+# create a pattern to select only columns with mean or std in their name
+pattern = '(mean|std)\\(\\)'
+# execute a pattern-matching function on features data to create an index vector
+ind = grep(pattern, features$featureLabel, perl=T)
+# use this index vector to extract only columns names I will need
+selected_features = as.vector(paste("V", features[ind,1],sep=""))
 
-# je ne suis pas sure de comprendre => calculer les mean/standard dev pour chaque colonne? 
-# ou filtrer uniquement les lignes pour lesquelles on est dans la moyenne/ecarttype?
+all<-cbind(all[,c(1:2)],all[,c(selected_features)])
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Uses descriptive activity names to name the activities in the data set
@@ -59,8 +64,7 @@ all<-rbind(test,train)
 
 #load data.table package to be able to add activityLabel through a join on activityCode
 install.packages("data.table")
-#library("data.table", lib.loc="D:/Program Files/R/R-3.0.3/library") #pc
-library("data.table", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")#mac
+library("data.table", lib.loc="D:/Program Files/R/R-3.0.3/library") #pc
 
 # put both data and labels in data.table format
 all_dt<-data.table(all)
@@ -74,30 +78,43 @@ setkey(activity_labels_dt,activityCode)
 all_dt<-merge(all_dt,activity_labels,by="activityCode")
 
 #delete work tables to clean workspace
-rm(subject_test);rm(X_test);rm(Y_test);rm(subject_train);rm(X_train);rm(Y_train);rm(test);rm(train);rm(all) 
+rm(subject_test);rm(X_test);rm(Y_test);rm(subject_train);rm(X_train);rm(Y_train);rm(test);rm(train);rm(all) ;rm(activity_labels)
+#remove unused activityCode column
+all_dt$activityCode <- NULL
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Appropriately labels the data set with descriptive variable names. 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-varNames<-as.vector(features[,2])
-names(all_dt)<-c("activityCode","subject",varNames,"activityLabel")
+varNames<-as.vector(features[ind,1])
+names(all_dt)<-c("subject",varNames,"activityLabel")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-#remove unused activityCode column
-all_dt$activityCode <- NULL
-
-#moyenne de chaque variable selon activity et subject
 
 #load reshape package to be able to tidy and do calculation on dataset
-install.packages("reshape")
-library("reshape", lib.loc="/Library/Frameworks/R.framework/Versions/3.1/Resources/library")
+install.packages("reshape2")
+library("reshape2", lib.loc="D:/Program Files/R/R-3.0.3/library") #pc
+
 
 # first, use melt to have a dataset with columns (activityLabel, subject, variable, value)
 tidy0<-melt(all_dt,id=c("activityLabel","subject"))
-# second, use cast to calculate mean of each value, grouped by (activityLabel, subject)
-tidy<-cast(tidy0, activityLabel +  subject ~ value , mean)
+rm(all_dt)
 
+# second, use cast to calculate mean of each value, grouped by (activityLabel, subject)
+
+# put data in data.table format
+tidy0<-data.table(tidy0)
+#set indexes
+setkey(tidy0,activityLabel,subject)
+
+startTime<-date()
+tidy<-cast(tidy0, activityLabel +  subject ~ value , mean)
+endTime<-date()
+calcTime<-endTime-startTime
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# export tidy data to a txt file
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
+write.table(tidy, "./tidy.txt", sep=";")
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
